cfeea55ce2425833283819f4231a2cf3e029e92c
Charles Essien <cessien88@gmail.com>
2021-02-06 12:00:21 +0000

    basic feature flag sketch

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,15 +1,23 @@
 import './App.css';
 import React from 'react';
 import Avatar from '@material-ui/core/Avatar';
-import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Grid from '@material-ui/core/Grid';
 import FlagIcon from '@material-ui/icons/FlagOutlined';
+import GroupIcon from '@material-ui/icons/Group';
+import PersonIcon from '@material-ui/icons/Person';
+import NoPersonIcon from '@material-ui/icons/NotInterested';
+import FilterListIcon from '@material-ui/icons/FilterList';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
+import Box from '@material-ui/core/Box';
 import { List, ListItem } from '@material-ui/core';
 import Switch from '@material-ui/core/Switch';
+import LinearProgress from '@material-ui/core/LinearProgress';
+import FormGroup from '@material-ui/core/FormGroup';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Chip from '@material-ui/core/Chip';
 
 const useStyles = makeStyles((theme) => ({
   paper: {
@@ -23,14 +31,26 @@ const useStyles = makeStyles((theme) => ({
     backgroundColor: theme.palette.secondary.main,
   },
   featureList: {
-    width: "40vmin",
+    width: "100vmin",
   },
+  featureDescription: {
+    color: "#9C9C9C",
+  },
+  featureFilterList: {
+    display: 'flex',
+    flexDirection: 'row',
+    padding: 0,
+  },
+  featureFilterListItem: {
+    paddingLeft: '0em',
+    paddingRight: '.5em'
+  }
 }));
 
 const features = [
   {
     key: "pr.ui-designer-hint",
-    description: "Users get hint notifications to tag UI desingers on PRs",
+    description: "Users get hint notifications to tag UI designers on PRs",
     enabled: true,
     users: [
       "cessien",
@@ -39,7 +59,23 @@ const features = [
     ],
     groups: [
       "early-adopters"
-    ]
+    ],
+    percentage: 75
+  },
+  {
+    key: "app.some-feature",
+    description: "Some feature we want to control",
+    enabled: true,
+    users: [
+      "hermione",
+      "sma",
+      "-flam",
+      "remi",
+      "-scarface"
+    ],
+    groups: [
+    ],
+    percentage: 10
   },
 ]
 
@@ -56,7 +92,7 @@ export default function App() {
   };
 
   return (
-    <Container component="main" maxWidth="xs">
+    <Container component="main" maxWidth="lg">
       <CssBaseline />
       <div className={classes.paper}>
         <Avatar className={classes.avatar}>
@@ -67,32 +103,41 @@ export default function App() {
         </Typography>
         <List className={classes.featureList}>
           {features.map(l => {
-          return <ListItem key={l}>
+          return <ListItem key={l} divider>
             <Grid container spacing={2}>
-              <Grid item md={2}>
-                <Switch
-                  checked={state.checkedA}
-                  onChange={handleChange}
-                  name={state.checkedA ? "ON" : "DISABLED"}
-                  inputProps={{ 'aria-label': 'secondary checkbox' }}
-                />
+              <Grid item xs={2}>
+                <FormGroup>
+                  <FormControlLabel control={
+                    <Switch
+                      checked={l.enabled}
+                      onChange={handleChange}
+                    />
+                  } label={l.enabled ? "ON" : "OFF"} />
+                </FormGroup>
               </Grid>
-              <Grid item md={10}>
-                things
+              <Grid item xs={10}>
+                <Grid container spacing={2}>
+                  <Grid item xs={8}><Typography component="div"><Box fontWeight="bold">{l.key}</Box></Typography></Grid>
+                  <Grid item xs={4}><LinearProgress variant="determinate" value={l.percentage} /></Grid>
+                  <Grid item xs={12}><Typography component="div"><Box className={classes.featureDescription}>{l.description}</Box></Typography></Grid>
+                </Grid>
+              </Grid>
+              <Grid item xs={1}></Grid>
+              <Grid item xs={1}><FilterListIcon /></Grid>
+              <Grid item xs={10}>
+                <List className={classes.featureFilterList}>
+                  {l.groups.map(g => {
+                    return <ListItem className={classes.featureFilterListItem} key={g}><Chip icon={<GroupIcon />} color='primary' label={g} onDelete={console.log} /></ListItem>
+                  })}
+                  {l.users.map(u => {
+                    return <ListItem className={classes.featureFilterListItem} key={u}><Chip icon={u[0] === '-' ?<NoPersonIcon /> : <PersonIcon />} label={u.replace(/^-/,'')} onDelete={console.log} /></ListItem>
+                  })}
+                </List>
               </Grid>
             </Grid>
           </ListItem>
           })}
         </List>
-        <Button
-            type="submit"
-            fullWidth
-            variant="contained"
-            color="primary"
-            className={classes.submit}
-          >
-            Sign Up
-        </Button>
       </div>
     </Container>
   );
