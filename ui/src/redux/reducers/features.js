f8657dc73629fb73bbe17a1fd60c3bfc4b581b77
cessien <cessien@cargurus.com>
2021-02-16 22:33:00 -0500

    cleanup

--- ui/src/redux/reducers/features.js
+++ ui/src/redux/reducers/features.js
@@ -12,38 +12,6 @@ const initialState = {
   features: {},
 };
 
-let features = {
-    "pr_ui_designer_hint": {
-      key: "pr_ui_designer_hint",
-      description: "Users get hint notifications to tag UI designers on PRs",
-      enabled: true,
-      users: [
-        "cessien",
-        "sma",
-        "-flam"
-      ],
-      groups: [
-        "early-adopters"
-      ],
-      percentage: 75
-    },
-    "app_some_feature": {
-      key: "app_some_feature",
-      description: "Some feature we want to control",
-      enabled: false,
-      users: [
-        "hermione",
-        "sma",
-        "-flam",
-        "remi",
-        "-scarface"
-      ],
-      groups: [
-      ],
-      percentage: 10
-    },
-  }
-
 export default function(state = initialState, action) {
   switch (action.type) {
     case ADD_FEATURE: {
